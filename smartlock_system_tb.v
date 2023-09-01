717df21f66f0373bad04508a175c782fa087225f
Gowda07 <142581040+Gowda07@users.noreply.github.com>
2023-09-01 21:57:20 +0530

    Update smartlock_system_tb.v

--- smartlock_system_tb.v
+++ smartlock_system_tb.v
@@ -1,96 +1,33 @@
-module lock_fsm (enter, keyin, reset,
+module test_lock_fsm();
+reg clk = 0;
+reg [2:0] lock_key = 3'b110;
+wire valid;
+reg reset, enter, keyin;
+wire unlock, error;
+wire [3:0] state;
+//reding for validity
+read_valid read(valid);
+//actual program
+lock_fsm lock_obj(enter, keyin, reset,
 		 valid, lock_key, clk,
 		 unlock, error, state);
-input clk;
-input [2:0] lock_key;
-input reset, enter, keyin, valid;
-output unlock, error;
-output [3:0] state;
-reg [3:0] state, next_state;
-parameter [3:0] START = 4'b0000;
-parameter [3:0] cmp0 = 4'b0001;
-parameter [3:0] idle0 = 4'b0010;
-parameter [3:0] idle0x = 4'b0011;
-parameter [3:0] error1 = 4'b0100;
-parameter [3:0] cmp1 = 4'b0101;
-parameter [3:0] idle1 = 4'b0110;
-parameter [3:0] idle1x = 4'b0111;
-parameter [3:0] error2 = 4'b1000;
-parameter [3:0] cmp2 = 4'b1001;
-parameter [3:0] idle2 = 4'b1010;
-parameter [3:0] idle2x = 4'b1011;
-parameter [3:0] error3 = 4'b1100;
-parameter [3:0] done = 4'b1110;
-always @ (posedge clk)
+always #5 clk = ~clk;
+initial
 begin
-if (reset & valid)
-state <= START;
-else if (valid)
-state <= next_state;
+reset = 1;
+#10;
+reset = 0; enter = 1;
+#10;
+keyin = 1; enter = 0;
+#10;
+enter = 1;
+#10;
+keyin = 1; enter = 0;
+#10;
+enter = 1;
+#10;
+keyin = 0; enter = 0;
+#10;
+enter = 1;
 end
-
-not n_ent (n_enter, enter);
-not n_res (n_reset, reset);
-
-always @ *
-begin
-case (state)
-START:
-if (reset  & n_enter)
-next_state <= START;
-else if (n_reset & enter)
-next_state <= cmp0;
-cmp0:
-if (keyin == lock_key[2])
-next_state <= idle0;
-else
-next_state <= idle0x;
-idle0:
-if (n_enter)
-next_state <= idle0;
-else
-next_state <= cmp1;
-idle0x:
-if (n_enter)
-next_state <= idle0x;
-else
-next_state <= error1;
-error1:
-next_state <= idle1x;
-cmp1:
-if (keyin == lock_key[1])
-next_state <= idle1;
-else
-next_state <= idle1x;
-idle1:
-if (n_enter)
-next_state <= idle1;
-else
-next_state <= cmp2;
-idle1x:
-if (n_enter)
-next_state <= idle1x;
-else
-next_state <= error2;
-error2:
-next_state <= idle2x;
-cmp2:
-if (keyin == lock_key[0])
-next_state <= idle2;
-else
-next_state <= idle2x;
-idle2:
-if (n_enter)
-next_state <= idle2;
-else
-next_state <= done;
-idle2x:
-if (n_enter)
-next_state <= idle2x;
-else
-next_state <= error3;
-endcase
-end
-assign unlock = (state == done);
-assign error = (state == error3);
 endmodule
